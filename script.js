786b1bf5ca344f7e101ccbbc7ce8d5059f621f02
Bekmirzo <99716618+Bekmirzo@users.noreply.github.com>
2022-03-01 20:58:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+var d0 = document.getElementsByClassName('d0')[0];
+var d1 = document.getElementsByClassName('d1')[0];
+var d2 = document.getElementsByClassName('d2')[0];
+
+var styled1 = window.getComputedStyle(d1);
+var styled2 = window.getComputedStyle(d2);
 
